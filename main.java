04ffcba8234005eb0ce854f9f91850503556f077
Sümeyye <58435258+svcsmy@users.noreply.github.com>
2023-08-13 18:12:26 +0300

    Add files via upload

---
+++ main.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class main {
+	
+	public static void toplama(String a, String b) {
+		System.out.println(a+b);
+	}
+	
+	public static void toplama(int a, int b) {
+		System.out.println(a+b);
+	}
+	
+	
+	public static void toplama(int a, int b, int c) {
+		System.out.println(a+b+c);
+	}
+	
+	public static void main(String[] args) {
+ 
+     toplama(3,4,5);
+     toplama(2,5);
+	 toplama("over","loading");
+		
+		
+		
+		
+		
+	}
+
+}
